810c801cb56dd3d81abfbe693b2a6aa6ad0f349b
pizzapup <hardwickariella@gmail.com>
2023-01-14 11:31:27 -0500

    saving to deploy

--- src/components/PeopleCards/Carlisha.js
+++ src/components/PeopleCards/Carlisha.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import CarlishaW from '../../assets/profilepics/CarlishaWilson.JPG'
+import React from "react";
+
+import CarlishaW from "../../assets/profilepics/Placeholder2.png";
 import {
   Container,
   Col,
@@ -9,46 +10,45 @@ import {
   CardText,
   Button,
   CardSubtitle,
-  CardBody
-} from 'reactstrap';
-import Github from '../../assets/icons/github.png'
-import LinkedIn from '../../assets/icons/linkedin.png'
-import useCollapse from 'react-collapsed';
-
+  CardBody,
+} from "reactstrap";
+import Github from "../../assets/icons/github.png";
+import LinkedIn from "../../assets/icons/linkedin.png";
 
 const Carlisha = () => {
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
-
   return (
     <div>
       <Card
         style={{
-          width: '18rem',
-          border: '1rem solid #70BA89'
+          width: "18rem",
+          border: "1rem solid #70BA89",
         }}
       >
-        <div style={{
-          width: '256px',
-          height: '256px',
-          position: 'relative',
-          overflow: 'hidden',
-          borderRadius: '50%'
-        }}>
+        <div
+          style={{
+            width: "256px",
+            height: "256px",
+            position: "relative",
+            overflow: "hidden",
+            borderRadius: "50%",
+          }}
+        >
           <img
             src={CarlishaW}
             alt="Profile Picture"
             style={{
-              display: 'inline',
-              margin: '0 auto',
-              height: 'auto',
-              width: '100%'
+              display: "inline",
+              margin: "0 auto",
+              height: "auto",
+              width: "100%",
             }}
           />
         </div>
         <CardBody>
-          <CardTitle tag="h3"
+          <CardTitle
+            tag="h3"
             style={{
-              fontFamily: 'Vibur, cursive',
+              fontFamily: "Vibur, cursive",
             }}
           >
             Carlisha Wilson
@@ -57,37 +57,37 @@ const Carlisha = () => {
             className="mb-2 text-muted"
             tag="h5"
             style={{
-              fontFamily: 'Vibur, cursive',
+              fontFamily: "Vibur, cursive",
             }}
           >
             UX/UI, Design
           </CardSubtitle>
-          <div
-            className="collapsible"
-          >
-            <div 
-              style={{
-                fontFamily: 'Vibur, cursive',
-              }}
-              className="header text-primary mb-3" {...getToggleProps()}>
-              {isExpanded ? 'Read Less' : 'Read More'}
-            </div>
-            <div {...getCollapseProps()}>
+          <details className="collapsible description" open>
+            <summary data-open="read more" data-close="read less"></summary>
+            <div>
               <div className="content">
                 <CardText className="mb-3">
-                  Carlisha is a Digital Artist studying to be a UX Researcher. When she is not studying, her time as a freelance photographer is spent empathizing with her clientele and creating a safe space for anyone who finds themselves in front of her camera.
+                  Carlisha is a Digital Artist studying to be a UX Researcher.
+                  When she is not studying, her time as a freelance photographer
+                  is spent empathizing with her clientele and creating a safe
+                  space for anyone who finds themselves in front of her camera.
                 </CardText>
               </div>
             </div>
-          </div>
-          <a href="http://linkedin.com/in/carlishawilson"><img src={LinkedIn} alt="LinkedIn"
-            style={{
-              width: '3rem'
-            }} /></a>
+          </details>
+          <a href="http://linkedin.com/in/carlishawilson">
+            <img
+              src={LinkedIn}
+              alt="LinkedIn"
+              style={{
+                width: "3rem",
+              }}
+            />
+          </a>
         </CardBody>
-      </Card >
-    </div >
-  )
-}
+      </Card>
+    </div>
+  );
+};
 
-export default Carlisha
+export default Carlisha;
--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,189 +1,157 @@
-// import Github from "../assets/icons/github.png";
-// import LinkedIn from "../assets/icons/linkedin.png";
-// import Lucie from "../components/PeopleCards/Lucie";
-// import Christine from "../components/PeopleCards/Christine";
-// import Polina from "../components/PeopleCards/Polina";
-// import Lindsay from "../components/PeopleCards/Lindsay";
-// import Christina from "../components/PeopleCards/Christina";
-// import Kimberly from "../components/PeopleCards/Kimberly";
-// import Gauri from "../components/PeopleCards/Gauri";
-// import Mollie from "../components/PeopleCards/Mollie";
-// import Carlisha from "../components/PeopleCards/Carlisha";
-// import Bowl from "../assets/imgs/bowl.jpg";
-// import Pie from "../assets/imgs/toast.jpg";
+import Github from "../assets/icons/github.png";
+import LinkedIn from "../assets/icons/linkedin.png";
+import Carlisha from "../components/PeopleCards/Carlisha";
+import Bowl from "../assets/imgs/bowl.jpg";
+import Pie from "../assets/imgs/toast.jpg";
 import { useNavigate } from "react-router-dom";
 
-// import {
-//   Container,
-//   Col,
-//   Row,
-//   Card,
-//   CardTitle,
-//   CardText,
-//   Button,
-//   CardSubtitle,
-//   CardBody,
-// } from "reactstrap";
+import {
+  Container,
+  Col,
+  Row,
+  Card,
+  CardTitle,
+  CardText,
+  Button,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
 
 const AboutUsPage = () => {
-  return <>About Us</>;
-  //   const navigate = useNavigate();
-  //   return (
-  //     <Container>
-  //       <Card
-  //         className="mb-3"
-  //         style={{
-  //           border: "1rem solid #70BA89",
-  //         }}
-  //       >
-  //         <Row>
-  //           <Col className="d-flex flex-column align-items-center justify-content-center">
-  //             <div className="p-5">
-  //               <h2
-  //                 style={{
-  //                   fontFamily: "Vibur, cursive",
-  //                 }}
-  //               >
-  //                 Our Mission
-  //               </h2>
-  //               <p>
-  //                 "Note to Chef" makes adjusting for dietary accommodations simple
-  //                 by bringing common restrictions to the forefront of the recipe
-  //                 search experience. From the home page, select any number of
-  //                 overlapping dietary restrictions and a curated list of 10 top
-  //                 recipes will populate!
-  //               </p>
-  //               <p>
-  //                 If you find a recipe you enjoy, you can register for an account
-  //                 and save them to your profile for future use. You can also store
-  //                 recipes in a collection to make it easy to account for diverse
-  //                 dietary needs when planning for large gatherings. Additionally,
-  //                 a notes feature for saved recipes provides the ability to list
-  //                 adaptations to the recipe, and track details on each attempt.{" "}
-  //               </p>
-  //               <p>
-  //                 "Note to Chef" makes it easy to take into consideration the
-  //                 unique dietary needs of yourself, and your guests!
-  //               </p>
-  //             </div>
-  //           </Col>
-  //           <Col className="col-auto d-flex flex-column justify-content-center align-items-center">
-  //             <div>
-  //               <img
-  //                 src={Bowl}
-  //                 alt="Bowl"
-  //                 style={{
-  //                   maxWidth: "30rem",
-  //                 }}
-  //               />
-  //             </div>
-  //           </Col>
-  //         </Row>
-  //       </Card>
-  //       <Row className="d-flex align-items-center justify-content-center">
-  //         <h2
-  //           className="text-center mt-5 mb-3"
-  //           style={{
-  //             fontFamily: "Vibur, cursive",
-  //           }}
-  //         >
-  //           Our Team
-  //         </h2>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Christine />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Carlisha />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Lucie />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Christina />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Kimberly />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Gauri />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Mollie />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Polina />
-  //         </Col>
-  //         <Col className="d-flex align-items-center justify-content-center mb-3">
-  //           <Lindsay />
-  //         </Col>
-  //       </Row>
-  //       <Card
-  //         className="mb-3"
-  //         style={{
-  //           border: "1rem solid #70BA89",
-  //           borderBottom: "none",
-  //         }}
-  //       >
-  //         <Row>
-  //           <Col className="col-auto d-flex flex-column justify-content-center align-items-center">
-  //             <img
-  //               src={Pie}
-  //               alt="Pie"
-  //               style={{
-  //                 maxWidth: "30rem",
-  //               }}
-  //             />
-  //           </Col>
-  //           <Col className="d-flex flex-column align-items-center justify-content-center">
-  //             <div className="p-5">
-  //               <h2
-  //                 style={{
-  //                   fontFamily: "Vibur, cursive",
-  //                 }}
-  //               >
-  //                 About Us
-  //               </h2>
-  //               <p>
-  //                 Our team came together around a passion for nutrition and
-  //                 inclusion for diverse dietary needs during the GirlDevelopIt 2nd
-  //                 Annual Virtual Hackathon 2022.
-  //               </p>
-  //               <p>
-  //                 {" "}
-  //                 Over the course of three days, we developed our application
-  //                 idea, coordinated teams for the frontend and backend, met in
-  //                 daily stand-ups, designed in Figma, worked asynchronously across
-  //                 timezones, thought through the user experience and built a
-  //                 close-knit community.{" "}
-  //               </p>
-  //               <p>
-  //                 All of us contributed to the success of this project, and we're
-  //                 pleased to present to you "Note to Chef"!
-  //               </p>
-  //             </div>
-  //           </Col>
-  //         </Row>
-  //         <div
-  //           className="text-end"
-  //           style={{
-  //             backgroundColor: "#70BA89",
-  //           }}
-  //         >
-  //           <a href="https://github.com/tinalr/recipebox-gdihackathon2022">
-  //             <button
-  //               className="btn text-white hover-dark"
-  //               style={{
-  //                 fontFamily: "Vibur, cursive",
-  //                 backgroundColor: "#70BA89",
-  //               }}
-  //             >
-  //               GITHUB REPOSITORY
-  //             </button>
-  //           </a>
-  //         </div>
-  //       </Card>
-  //     </Container>
-  //   );
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Card
+        className="mb-3"
+        style={{
+          border: "1rem solid #70BA89",
+        }}
+      >
+        <Row>
+          <Col className="d-flex flex-column align-items-center justify-content-center">
+            <div className="p-5">
+              <h2
+                style={{
+                  fontFamily: "Vibur, cursive",
+                }}
+              >
+                Our Mission
+              </h2>
+              <p>
+                "Note to Chef" makes adjusting for dietary accommodations simple
+                by bringing common restrictions to the forefront of the recipe
+                search experience. From the home page, select any number of
+                overlapping dietary restrictions and a curated list of 10 top
+                recipes will populate!
+              </p>
+              <p>
+                If you find a recipe you enjoy, you can register for an account
+                and save them to your profile for future use. You can also store
+                recipes in a collection to make it easy to account for diverse
+                dietary needs when planning for large gatherings. Additionally,
+                a notes feature for saved recipes provides the ability to list
+                adaptations to the recipe, and track details on each attempt.{" "}
+              </p>
+              <p>
+                "Note to Chef" makes it easy to take into consideration the
+                unique dietary needs of yourself, and your guests!
+              </p>
+            </div>
+          </Col>
+          <Col className="col-auto d-flex flex-column justify-content-center align-items-center">
+            <div>
+              <img
+                src={Bowl}
+                alt="Bowl"
+                style={{
+                  maxWidth: "30rem",
+                }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Card>
+      <Row className="d-flex align-items-center justify-content-center">
+        <h2
+          className="text-center mt-5 mb-3"
+          style={{
+            fontFamily: "Vibur, cursive",
+          }}
+        >
+          Our Team
+        </h2>
+
+        <Col className="d-flex align-items-center justify-content-center mb-3">
+          <Carlisha />
+        </Col>
+      </Row>
+      <Card
+        className="mb-3"
+        style={{
+          border: "1rem solid #70BA89",
+          borderBottom: "none",
+        }}
+      >
+        <Row>
+          <Col className="col-auto d-flex flex-column justify-content-center align-items-center">
+            <img
+              src={Pie}
+              alt="Pie"
+              style={{
+                maxWidth: "30rem",
+              }}
+            />
+          </Col>
+          <Col className="d-flex flex-column align-items-center justify-content-center">
+            <div className="p-5">
+              <h2
+                style={{
+                  fontFamily: "Vibur, cursive",
+                }}
+              >
+                About Us
+              </h2>
+              <p>
+                Our team came together around a passion for nutrition and
+                inclusion for diverse dietary needs during the GirlDevelopIt 2nd
+                Annual Virtual Hackathon 2022.
+              </p>
+              <p>
+                {" "}
+                Over the course of three days, we developed our application
+                idea, coordinated teams for the frontend and backend, met in
+                daily stand-ups, designed in Figma, worked asynchronously across
+                timezones, thought through the user experience and built a
+                close-knit community.{" "}
+              </p>
+              <p>
+                All of us contributed to the success of this project, and we're
+                pleased to present to you "Note to Chef"!
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <div
+          className="text-end"
+          style={{
+            backgroundColor: "#70BA89",
+          }}
+        >
+          <a href="https://github.com/tinalr/recipebox-gdihackathon2022">
+            <button
+              className="btn text-white hover-dark"
+              style={{
+                fontFamily: "Vibur, cursive",
+                backgroundColor: "#70BA89",
+              }}
+            >
+              GITHUB REPOSITORY
+            </button>
+          </a>
+        </div>
+      </Card>
+    </Container>
+  );
 };
 
 export default AboutUsPage;
